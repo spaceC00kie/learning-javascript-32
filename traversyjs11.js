74266c567b09ba03007bb49bb09b576f14041e9c
spaceC00kie <kirstie317@gmail.com>
2020-12-23 18:35:20 -0500

    loop through array

---
+++ traversyjs11.js
@@ -0,0 +1,41 @@
+//array of objects
+
+const todos = [
+  {
+    id: 1,
+    text: "Take out trash",
+    isCompleted: true,
+  },
+  {
+    id: 2,
+    text: "Do laundry",
+    isCompleted: true,
+  },
+  {
+    id: 3,
+    text: "Load dishwasher",
+    isCompleted: false,
+  },
+]
+
+// LOOP THROUGH ARRAY
+//  returns only text info
+//for (let i = 0; i < todos.length; i++) {
+//  console.log(todos[i].text)
+//}
+
+//  returns all info about the item
+//for (let todo of todos) {
+//  console.log(todo)
+//}
+
+//  returns text about the item a different way
+//for (let todo of todos) {
+//  console.log(todo.text)
+//}
+
+//  returns id of the item
+//for (let todo of todos) {
+//  console.log(todo.id)
+//}
+
